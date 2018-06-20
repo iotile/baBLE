d16afd3d27f4256e7d4b86301f6942786e47a710
Alexis ANNEIX <aanneix@arch-iot.com>
2018-06-20 15:40:23 -0700

    Write setup.py + remove dev files

---
+++ interfaces/python/README.md
@@ -0,0 +1,3 @@
+# baBLE - python interface
+
+This is the Python interface to interact with the baBLE bridges.
--- interfaces/python/setup.py
+++ interfaces/python/setup.py
@@ -1,7 +1,20 @@
 from setuptools import setup, find_packages
+import sys
 import version
 
-# TODO: according to the OS, import baBLE executable
+# Make sure the system has the right Python version.
+if sys.version_info < (2, 7):
+    print("baBLE requires Python 2.7 or newer.")
+    sys.exit(1)
+
+# Check the OS
+if sys.platform == 'linux':
+    # TODO: add possibility to use either the precompiled executable in the wheel or the user compiled (in PATH)
+    pass
+else:
+    print("baBLE only works on Linux for now.")
+    sys.exit(1)
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
